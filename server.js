595208f79d7c65820b371490bf2cd8c5ae8156cb
matisapatta <mati.sapatta@gmail.com>
2020-01-05 19:13:41 -0300

    .

--- server.js
+++ server.js
@@ -1052,7 +1052,7 @@ app.get('/api/latestsala', (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   const path = require('path');
   app.get('/*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'../client','build','index.html'));
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
   });
 
 }
